9e1c6aab4e83d0b3d652ef23102b0037b054a8d8
Diegoo11 <ynoacamino@gmail.com>
2023-07-07 17:23:20 -0500

    homework

---
+++ Avance3_YenaroNoa.java
@@ -0,0 +1,16 @@
+class Avance3_YenaroNoa {
+  public static void main(String[] args) {
+    String[] me = {
+      "Yenaro", "Joel", "Noa", "Camino"
+    };
+
+    String[] me2 = new String[4];
+    me2[0] = "Yenaro";
+    me2[1] = "Joel";
+    me2[2] = "Noa";
+    me2[3] = "Camino";
+
+    System.out.println(me[1]+ me2[1]);
+
+  }
+}
