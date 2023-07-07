2d4275e5ce2c008018d202e80240eb44bd1ff0cb
Diegoo11 <ynoacamino@gmail.com>
2023-07-07 17:22:55 -0500

    homework

---
+++ Arreglos3.java
@@ -0,0 +1,8 @@
+class Arreglos3 {
+  public static void main(String[] args) {
+    int array [] = new int [5];
+    for(int i = 0; i < 5; i += 1) {
+      System.out.println(array[i]);
+    }
+  }
+}
